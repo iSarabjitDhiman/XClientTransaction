f33670478bf799c2c10b33894c97347ccb9904a1
Sarabjit Dhiman <isarabjitdhiman@gmail.com>
2026-06-26 16:16:55 +0530

    Bump Version to 1.0.3
    Bump PyPi version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 SHORT_DESCRIPTION = "Twitter X-Client-Transaction-Id generator written in python."
 
 with open("requirements.txt") as file:
